c72ecfc534774724f01d261d40a0f5a6d7d90bb3
Jochen Voss <voss@seehuhn.de>
2023-03-20 23:21:51 +0000

    updates

--- outline/outline.go
+++ outline/outline.go
@@ -1,3 +1,19 @@
+// seehuhn.de/go/pdf - a library for reading and writing PDF files
+// Copyright (C) 2023  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package outline
 
 import (
@@ -10,7 +26,16 @@ import (
 type Tree struct {
 	Title    string
 	Children []*Tree
-	Count    pdf.Integer
+	Open     bool
+	Action   pdf.Dict
+}
+
+func (tree *Tree) AddChild(title string) *Tree {
+	child := &Tree{
+		Title: title,
+	}
+	tree.Children = append(tree.Children, child)
+	return child
 }
 
 func Read(r *pdf.Reader) (*Tree, error) {
@@ -20,14 +45,15 @@ func Read(r *pdf.Reader) (*Tree, error) {
 	}
 
 	seen := map[*pdf.Reference]bool{}
-	tree, _, err := dr1(r, seen, root)
+	tree, _, err := readNode(r, seen, root)
 	if err != nil {
 		return nil, err
 	}
+	tree.Open = false
 	return tree, nil
 }
 
-func dr1(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) (*Tree, pdf.Dict, error) {
+func readNode(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) (*Tree, pdf.Dict, error) {
 	if seen[node] {
 		return nil, nil, fmt.Errorf("outline tree contains a loop")
 	}
@@ -54,10 +80,19 @@ func dr1(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) (*Tre
 	}
 
 	count, _ := dict["Count"].(pdf.Integer)
-	tree.Count = count
+	tree.Open = count > 0
+
+	if dest, _ := r.Resolve(dict["Dest"]); dest != nil {
+		tree.Action = pdf.Dict{
+			"S": pdf.Name("GoTo"),
+			"D": dest,
+		}
+	} else if a, _ := r.GetDict(dict["A"]); a != nil {
+		tree.Action = a
+	}
 
 	ccPtr, _ := dict["First"].(*pdf.Reference)
-	cc, err := dr2(r, seen, ccPtr)
+	cc, err := readChildren(r, seen, ccPtr)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -66,10 +101,10 @@ func dr1(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) (*Tre
 	return tree, dict, nil
 }
 
-func dr2(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) ([]*Tree, error) {
+func readChildren(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) ([]*Tree, error) {
 	var res []*Tree
 	for node != nil {
-		tree, dict, err := dr1(r, seen, node)
+		tree, dict, err := readNode(r, seen, node)
 		if err != nil {
 			return nil, err
 		}
@@ -80,3 +115,153 @@ func dr2(r *pdf.Reader, seen map[*pdf.Reference]bool, node *pdf.Reference) ([]*T
 	}
 	return res, nil
 }
+
+func (tree *Tree) Write(w *pdf.Writer) error {
+	if tree == nil {
+		return nil
+	}
+
+	ww := &writer{
+		out:   w,
+		root:  tree,
+		count: map[*Tree]int{},
+	}
+	ww.getCount(tree)
+	if !ww.hasOpen {
+		delete(ww.count, tree)
+	}
+
+	rootRef := w.Alloc()
+	rootDict := pdf.Dict{}
+	err := ww.writeNode(rootRef, rootDict, tree)
+	if err != nil {
+		return err
+	}
+
+	err = ww.flush()
+	if err != nil {
+		return err
+	}
+
+	w.Catalog.Outlines = rootRef
+	return nil
+}
+
+type writer struct {
+	out     *pdf.Writer
+	root    *Tree
+	refs    []*pdf.Reference
+	objs    []pdf.Object
+	count   map[*Tree]int
+	hasOpen bool
+}
+
+func (ww *writer) getCount(tree *Tree) int {
+	if tree == nil {
+		return 0
+	}
+	total := len(tree.Children)
+	for _, child := range tree.Children {
+		cCount := ww.getCount(child)
+		if cCount > 0 {
+			total += cCount
+		}
+	}
+	if !tree.Open && tree != ww.root {
+		total = -total
+	} else {
+		ww.hasOpen = true
+	}
+	if total != 0 && (ww.hasOpen || tree != ww.root) {
+		ww.count[tree] = total
+	}
+	return total
+}
+
+func (ww *writer) writeNode(ref *pdf.Reference, dict pdf.Dict, tree *Tree) error {
+	if tree.Title != "" && tree != ww.root {
+		dict["Title"] = pdf.TextString(tree.Title)
+	}
+	if tree.Action != nil && tree != ww.root {
+		if tree.Action["S"] == pdf.Name("GoTo") {
+			dict["Dest"] = tree.Action["D"]
+		} else {
+			dict["A"] = tree.Action
+		}
+	}
+
+	var first, last *pdf.Reference
+	if len(tree.Children) > 0 {
+		first = ww.out.Alloc()
+		if len(tree.Children) > 1 {
+			last = ww.out.Alloc()
+		} else {
+			last = first
+		}
+		dict["First"] = first
+		dict["Last"] = last
+		dict["Count"] = pdf.Integer(ww.count[tree])
+	}
+
+	ww.objs = append(ww.objs, dict)
+	ww.refs = append(ww.refs, ref)
+	if len(ww.objs) >= chunkSize {
+		err := ww.flush()
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(tree.Children) > 0 {
+		err := ww.writeChildren(ref, first, last, tree.Children)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ww *writer) writeChildren(parent, first, last *pdf.Reference, cc []*Tree) error {
+	ccRef := make([]*pdf.Reference, len(cc))
+	for i := range cc {
+		if i == 0 {
+			ccRef[i] = first
+		} else if i == len(cc)-1 {
+			ccRef[i] = last
+		} else {
+			ccRef[i] = ww.out.Alloc()
+		}
+	}
+
+	for i, tree := range cc {
+		dict := pdf.Dict{
+			"Parent": parent,
+		}
+		if i > 0 {
+			dict["Prev"] = ccRef[i-1]
+		}
+		if i < len(cc)-1 {
+			dict["Next"] = ccRef[i+1]
+		}
+		err := ww.writeNode(ccRef[i], dict, tree)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ww *writer) flush() error {
+	if len(ww.objs) == 0 {
+		return nil
+	}
+	_, err := ww.out.WriteCompressed(ww.refs, ww.objs...)
+	if err != nil {
+		return err
+	}
+	ww.refs = ww.refs[:0]
+	ww.objs = ww.objs[:0]
+	return nil
+}
+
+const chunkSize = 16
--- outline/outline_test.go
+++ outline/outline_test.go
@@ -1,3 +1,19 @@
+// seehuhn.de/go/pdf - a library for reading and writing PDF files
+// Copyright (C) 2023  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package outline
 
 import (
@@ -5,6 +21,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"seehuhn.de/go/pdf"
 	"seehuhn.de/go/pdf/document"
 )
@@ -24,6 +41,21 @@ func TestRead(t *testing.T) {
 	printTree(o, "")
 }
 
+func printTree(node *Tree, pfx string) {
+	if node == nil {
+		fmt.Printf("%snull\n", pfx)
+		return
+	}
+	if node.Title != "" {
+		fmt.Printf("%s%s (%t) %s\n", pfx, node.Title, node.Open, node.Action)
+	} else {
+		fmt.Printf("%s<no title> (%t) %s\n", pfx, node.Open, node.Action)
+	}
+	for _, c := range node.Children {
+		printTree(c, pfx+"  ")
+	}
+}
+
 func TestReadLoop(t *testing.T) {
 	buf := &bytes.Buffer{}
 
@@ -36,7 +68,6 @@ func TestReadLoop(t *testing.T) {
 
 		out := doc.Out
 
-		// create a loop in the outline tree
 		refRoot := out.Alloc()
 		refA := out.Alloc()
 		refB := out.Alloc()
@@ -50,7 +81,9 @@ func TestReadLoop(t *testing.T) {
 				"Parent": refRoot,
 			}
 		} else {
-			// Apparently this causes Acrobat reader to hang (version 2022.003.20310).
+			// Create a loop in the outline tree.
+			// This causes Acrobat reader to hang (version 2022.003.20310).
+			// Let's make sure we do better.
 			A = pdf.Dict{
 				"Title":  pdf.TextString("A"),
 				"Next":   refA,
@@ -112,17 +145,67 @@ func TestReadLoop(t *testing.T) {
 	}
 }
 
-func printTree(node *Tree, pfx string) {
-	if node == nil {
-		fmt.Printf("%snull\n", pfx)
-		return
+func TestWrite(t *testing.T) {
+	buf := &bytes.Buffer{}
+	doc, err := document.WriteSinglePage(buf, 100, 100)
+	if err != nil {
+		t.Fatal(err)
 	}
-	if node.Title != "" {
-		fmt.Printf("%s%s (%d)\n", pfx, node.Title, node.Count)
-	} else {
-		fmt.Printf("%s<no title> (%d)\n", pfx, node.Count)
+
+	tree1 := &Tree{
+		Children: []*Tree{
+			{
+				Title: "A",
+				Children: []*Tree{
+					{Title: "A1"},
+					{Title: "A2"},
+					{Title: "A3"},
+				},
+			},
+			{
+				Title: "B",
+				Action: pdf.Dict{
+					"S":   pdf.Name("URI"),
+					"URI": pdf.String("https://seehuhn.de/"),
+				},
+			},
+			{
+				Title: "C",
+				Children: []*Tree{
+					{Title: "C1"},
+					{Title: "C2"},
+				},
+				Open: true,
+			},
+		},
 	}
-	for _, c := range node.Children {
-		printTree(c, pfx+"  ")
+	err = tree1.Write(doc.Out)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	err = doc.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// err = os.WriteFile("test_Write.pdf", buf.Bytes(), 0644)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+
+	r, err := pdf.NewReader(bytes.NewReader(buf.Bytes()), int64(buf.Len()), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tree2, err := Read(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if d := cmp.Diff(tree1, tree2); d != "" {
+		t.Errorf("diff: %s", d)
+	}
+	printTree(tree2, "")
 }
