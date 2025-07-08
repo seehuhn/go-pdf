fc91bc9f7ecfe732822927282c942eb62def4763
Jochen Voss <voss@seehuhn.de>
2025-07-08 02:04:23 +0100

    doc fixes

---
+++ annotation/doc.go
@@ -0,0 +1,26 @@
+// seehuhn.de/go/pdf - a library for reading and writing PDF files
+// Copyright (C) 2025  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Package annotation provides types and functions for reading and writing PDF
+// annotations following the PDF specification.
+//
+// This package supports various annotation types including text annotations,
+// links, markup annotations (highlighting, underlining, strikeouts, etc.),
+// geometric shapes (squares, circles, polygons), and form widgets.
+//
+// All annotation types implement the [pdf.Annotation] interface. The function
+// [Extract] can be used to extract annotations from a PDF document.
+package annotation
--- annotation/text.go
+++ annotation/text.go
@@ -1,3 +1,19 @@
+// seehuhn.de/go/pdf - a library for reading and writing PDF files
+// Copyright (C) 2025  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package annotation
 
 import "seehuhn.de/go/pdf"
@@ -27,7 +43,7 @@ type Text struct {
 
 var _ pdf.Annotation = (*Text)(nil)
 
-// AnnotationType returns "text".
+// AnnotationType returns "Text".
 // This implements the [pdf.Annotation] interface.
 func (t *Text) AnnotationType() string {
 	return "Text"
